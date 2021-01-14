ec8a9a4e664668063e69ae97385b2c6e275b813d
PilotJinix <agungsantoso557@gmail.com>
2021-01-14 12:22:49 +0700

    login

--- lib/Details.dart
+++ lib/Details.dart
@@ -5,6 +5,8 @@ import 'package:suhamv1_app/Home.dart';
 import 'package:suhamv1_app/PageTwo.dart';
 import 'package:suhamv1_app/data.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:mapbox_gl/mapbox_gl.dart' as mb;
+import 'dart:math' as mathdart;
 
 
 class Detail extends StatefulWidget{
@@ -12,6 +14,41 @@ class Detail extends StatefulWidget{
   _DetailState createState() => _DetailState();
 }
 class _DetailState extends State<Detail> {
+  var token = 'pk.eyJ1IjoicGlsb3RqaW5peCIsImEiOiJja2p2c2Q0N24wODFqMndtbHlod2NvNnI0In0.TueBgQ2_KUtH4mnlVH8gyQ';
+  mb.MapboxMapController mapController;
+
+  Widget buildMap() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12),
+      height: 200,
+      child: mb.MapboxMap(
+        accessToken: token,
+        styleString: 'mapbox://styles/mapbox/streets-v11',
+        initialCameraPosition: new mb.CameraPosition(
+          target: mb.LatLng(-7.798804, 110.327649),
+          zoom: 12,
+        ),
+        onMapCreated: mapCreated,
+      ),
+    );
+  }
+
+  void mapCreated(mb.MapboxMapController controller) {
+    print("Ini controller ${controller}");
+    print("Ini controller ${mb.MapboxMapController}");
+    mapController = controller;
+    print('map created');
+
+    var lokasi = mb.LatLng(-7.798804, 110.327649);
+    print('lat lng clicked: ${lokasi.latitude}, ${lokasi.longitude}');
+    mapController.addCircle(new mb.CircleOptions(
+      circleRadius: 8,
+      circleColor: 'blue',
+      circleOpacity: 1,
+      geometry: lokasi,
+      draggable: false,
+    ));
+  }
 
   Widget gesergambar(){
     return Container(
@@ -101,6 +138,37 @@ class _DetailState extends State<Detail> {
                       fontSize: 11,
                       fontWeight: FontWeight.w300
                     ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Center(
+              child: Stack(
+                children: [
+                  buildMap(),
+                  Positioned(
+                      bottom: 5,
+                      right: 20,
+                      child: GestureDetector(
+                        onTap: ()=> Navigator.push(context , MaterialPageRoute(builder: (context) => Pagetwo())),
+                        child: Container(
+                          height: 35,
+                          width: 35,
+                          decoration: BoxDecoration(
+                            // shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 1,
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                            ),
+                            color: Colors.blue,
+                          ),
+                          child: Icon(
+                            Icons.map,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                   )
                 ],
               ),
--- lib/PageTwo.dart
+++ lib/PageTwo.dart
@@ -1,23 +1,109 @@
-import 'package:flutter/material.dart';
 
-class Pagetwo extends StatelessWidget{
+import 'dart:math' as mathdart;
+import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart' as mb;
+import 'package:geolocator/geolocator.dart' as geo;
+
+class Pagetwo extends StatefulWidget{
+  @override
+  _PagetwoState createState() => _PagetwoState();
+}
+
+class _PagetwoState extends State<Pagetwo> {
+  var token = 'pk.eyJ1IjoicGlsb3RqaW5peCIsImEiOiJja2p2c2Q0N24wODFqMndtbHlod2NvNnI0In0.TueBgQ2_KUtH4mnlVH8gyQ';
+
+  mb.MapboxMapController mapController;
+  mb.LatLng gpsLoc;
+  mb.Circle curPosCircle;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
-        title:Text ("Page Two"),
+        title:Text ("MAPS"),
         // iconTheme: new IconThemeData(color: Colors.blue),
-        actions: [
-          IconButton(
-            icon: Icon(
-                Icons.notifications_none
-            ),
-          )
-        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            buildMap(),
+            // buildBtnBottomRow(),
+          ],
+        ),
       ),
     );
+  }
 
+  Widget buildMap() {
+    return Container(
+      height: MediaQuery.of(context).size.height-80,
+      child: mb.MapboxMap(
+        accessToken: token,
+        styleString: 'mapbox://styles/mapbox/streets-v11',
+        initialCameraPosition: new mb.CameraPosition(
+          target: mb.LatLng(-7.798804, 110.327649),
+          zoom: 15,
+        ),
+
+        onMapCreated: mapCreated,
+        onMapClick: mapClicked,
+      ),
+    );
+  }
+
+  void mapClicked(mathdart.Point<double> point, mb.LatLng latlng) {
+    print('lat lng clicked: ${latlng.latitude}, ${latlng.longitude}');
+    if (mapController != null) {
+      mapController.addCircle(
+        new mb.CircleOptions(
+          circleRadius: 8,
+          circleColor: 'red',
+          circleOpacity: 1,
+          geometry: latlng,
+          draggable: false,
+        ),
+      );
+    }
+  }
+
+  void mapCreated(mb.MapboxMapController controller) {
+    print("Ini controller ${controller}");
+    print("Ini controller ${mb.MapboxMapController}");
+    mapController = controller;
+    print('map created');
+
+    var lokasi = mb.LatLng(-7.798804, 110.327649);
+    print('lat lng clicked: ${lokasi.latitude}, ${lokasi.longitude}');
+    mapController.addCircle(new mb.CircleOptions(
+      circleRadius: 8,
+      circleColor: 'blue',
+      circleOpacity: 1,
+      geometry: lokasi,
+      draggable: false,
+    ));
+  }
+
+  Widget buildBtnBottomRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        RaisedButton(
+          child: Text('Re-center'),
+          onPressed: () {
+            var center = new mb.LatLng(-8.1766639, 113.6916075);
+            var updatedLoc = mb.CameraUpdate.newLatLng(center);
+            if (mapController != null) {
+              mapController.animateCamera(updatedLoc);
+            } else {
+              print('map controller is null');
+            }
+          },
+        ),
+        RaisedButton(onPressed: () {}, child: Text('Btn2')),
+      ],
+    );
   }
 
 }
